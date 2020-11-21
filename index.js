640252544d578a9f8aeabe50215b4f7a7bada887
rahul <rahuldas35748@gmail.com>
2020-11-21 20:00:34 +0530

    something new

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require("body-parser")
 const {MONGOURL} = require('./config/keys')
+const path = require('path');
 
 require('dotenv').config();
 
@@ -32,7 +33,7 @@ app.use(require('./routes/user'));
 
 if(process.env.NODE_ENV=="production"){
   app.use(express.static('client/build'));
-  const path = require('path');
+
   app.get("*", (req, res) =>{
     res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
   })
